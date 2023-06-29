f3b776ec72130daf2f600b5d7eaaf2d9d59ffe52
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-06-29 20:05:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function showTextWithDelay(text, delayMs) {
+  await delay(delayMs);
+  console.log(text); // Display the text on the screen (change this line to suit your needs)
+}
+
+// Example usage
+showTextWithDelay("Hello, world!", 2000); // Display "Hello, world!" after a 2-second delay
